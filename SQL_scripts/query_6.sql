0e2f0bc7523419ca3931156f19bf0b4651eb5a43
Fleria <62247847+Fleria@users.noreply.github.com>
2022-06-05 02:41:57 +0300

    Update query_6.sql

--- SQL_scripts/query_6.sql
+++ SQL_scripts/query_6.sql
@@ -1,9 +1,9 @@
 select * from 
-	(select pr_researcher_id, count(*) as projcount 
+	(select pr_researcher_id, first_name ,last_name,count(*) as projcount 
     from 
 		(select proj_id 
         from projects 
-        where expiry_date>curdate()) active_proj 
+        where expiry_date>curdate() and start_date<curdate()) active_proj 
 	inner join projects_researchers on proj_id=pr_proj_id 
     inner join researchers on pr_researcher_id = researcher_id 
     where age<40 
@@ -11,12 +11,12 @@ select * from
 where projcount = 
 	(select max(projcount) as maximum 
 	from 
-		(select pr_researcher_id, count(*) as projcount 
+		(select pr_researcher_id,first_name,last_name, count(*) as projcount 
         from 
 			(select proj_id 
             from projects 
-            where expiry_date>curdate()) active_proj 
+            where expiry_date>curdate() and start_date<curdate()) active_proj 
 		inner join projects_researchers on proj_id=pr_proj_id 
-		inner join researchers on pr_researcher_id = researcher_id  
-		where age<40 group by pr_researcher_id order by projcount desc)b 
+		inner join researchers on pr_researcher_id = researcher_id 
+		where age<40 group by pr_researcher_id order by projcount desc)b
         );
