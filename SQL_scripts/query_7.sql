6d68accbab0e15fda928b185ecf6449fd9d70799
Fleria <62247847+Fleria@users.noreply.github.com>
2022-06-05 13:23:24 +0300

    Update query_7.sql

--- SQL_scripts/query_7.sql
+++ SQL_scripts/query_7.sql
@@ -2,6 +2,7 @@ select ename,organisation_name,sum(proj_amount) as total_amount
 from projects 
 inner join organisations on proj_organisation_id= organisation_id 
 inner join executives on proj_executive_id = executive_id 
+where category='comp' 
 group by proj_executive_id 
 order by sum(proj_amount) 
 desc limit 5;
