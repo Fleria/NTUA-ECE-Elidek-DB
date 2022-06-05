31ff29b09c69830b06f4d69a8ce44548559e9242
Fleria <62247847+Fleria@users.noreply.github.com>
2022-06-05 21:14:30 +0300

    Update query_1.sql

--- SQL_scripts/query_1.sql
+++ SQL_scripts/query_1.sql
@@ -1,3 +1,5 @@
 select * from projects 
 where duration = {duration} and proj_executive_id = {proj_executive_id} and start_date ={start_date} 
 ;
+
+/*this query does not work unless values are speicified*/
